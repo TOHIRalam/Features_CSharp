524fe955956114bc9475f54c2ab6453558afe3bb
Tasnimul Alam <tohiralam153@gmail.com>
2021-06-21 21:37:16 +0600

    Add project files.

---
+++ Features_CSharp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Features_CSharp
+{
+    class Delegate
+    {
+        public delegate int Perform(int[] arr);
+        public static int sum(int[] arr)
+        {
+            int sum = 0;
+            foreach (int i in arr)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+
+        public static void processMath(int[] arr, Perform perform)
+        {
+            perform(arr);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4 };
+            var logic = new Delegate.Perform(Delegate.sum);
+            Console.WriteLine(Delegate.sum(arr));
+        }
+    }
+}
