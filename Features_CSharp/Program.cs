9ed590e0ea9a36c4ad158499b7a9fee573f7f5e1
Tasnimul Alam <tohiralam153@gmail.com>
2021-06-24 04:54:41 +0600

    Updated

--- Features_CSharp/Program.cs
+++ Features_CSharp/Program.cs
@@ -2,30 +2,22 @@
 
 namespace Features_CSharp
 {
-    // Delegate
-    public delegate void Rectangle(double width, double height);
-    class Delegate
+    public struct car
     {
-        public void getArea(double width, double height)
-        {
-            Console.WriteLine($"Area of Rectangle: {width * height}");
-        }
-
-        public void getPerimeter(double width, double height)
-        {
-            Console.WriteLine($"Perimeter of Rectangle: {2 * (width * height)}");
-        }
+        public string color;
+        public string name;
+        public decimal price;
+        public string data() => color + ", " + name + ", " + price;
     }
-
     class Program
     {
         static void Main(string[] args)
         {
-            Delegate delegateClassObject = new Delegate();
-            Rectangle rectangle = new Rectangle(delegateClassObject.getArea);
-            rectangle += delegateClassObject.getPerimeter;
-
-            rectangle.Invoke(17.77, 40.22);
+            car car1 = new car();
+            car1.color = "Red";
+            car1.name = "XYZ";
+            car1.price = 10.56M;
+            Console.WriteLine(car1.data());
         }
     }
 }
