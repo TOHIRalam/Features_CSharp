ab6cacd95453c4bd2d95e61145cfa24b931c1c99
Tasnimul Alam <tohiralam153@gmail.com>
2021-06-24 04:35:50 +0600

    Updated

--- Features_CSharp/Program.cs
+++ Features_CSharp/Program.cs
@@ -2,30 +2,37 @@
 
 namespace Features_CSharp
 {
-    // Delegate
-    public delegate void Rectangle(double width, double height);
-    class Delegate
+    class MATH
     {
-        public void getArea(double width, double height)
+        public static int x { get; set; }
+        public static int y { get; set; }
+        public static int z { get; set; }
+
+        public MATH(int a, int b)
         {
-            Console.WriteLine($"Area of Rectangle: {width * height}");
+            x = a;
+            y = b;
         }
 
-        public void getPerimeter(double width, double height)
-        {
-            Console.WriteLine($"Perimeter of Rectangle: {2 * (width * height)}");
-        }
+        static MATH() { z = 10; }
+
+        public static int sum() => x + y;
+
+        public static int some_math() => z * (x + y);
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Delegate delegateClassObject = new Delegate();
-            Rectangle rectangle = new Rectangle(delegateClassObject.getArea);
-            rectangle += delegateClassObject.getPerimeter;
+            /*
+             * 1. Static constructor is used to initialize static fields
+             * 2. Static constructor can't have any modifiers
+             * 3. Static constructor is invoked implicitly
+            */
 
-            rectangle.Invoke(17.77, 40.22);
+            MATH math = new MATH(5, 6);
+            Console.WriteLine("Result: " + MATH.some_math()); // Output 110
         }
     }
 }
