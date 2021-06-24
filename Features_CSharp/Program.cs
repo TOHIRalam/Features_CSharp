337af6d2c258b03e51fa84b3be477b01d9c78564
Tasnimul Alam <tohiralam153@gmail.com>
2021-06-24 20:27:30 +0600

    Updated

--- Features_CSharp/Program.cs
+++ Features_CSharp/Program.cs
@@ -2,30 +2,68 @@
 
 namespace Features_CSharp
 {
-    // Delegate
-    public delegate void Rectangle(double width, double height);
-    class Delegate
+    public class DATA
     {
-        public void getArea(double width, double height)
+        public int x_axis;
+        public int y_axis;
+
+        public DATA(int x, int y)
         {
-            Console.WriteLine($"Area of Rectangle: {width * height}");
+            x_axis = x;
+            y_axis = y;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Constructor of the base class called");
+            Console.ResetColor();
         }
 
-        public void getPerimeter(double width, double height)
+        public void print_data()
         {
-            Console.WriteLine($"Perimeter of Rectangle: {2 * (width * height)}");
+            Console.WriteLine("Data");
         }
     }
 
+    // Single level inheritance
+    public class OPERATION : DATA
+    {
+        public OPERATION(int m, int n) : base(m, n)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Constructor of the operation class called");
+            Console.ResetColor();
+        }
+
+        public int sum() => x_axis + y_axis;
+        public int sub() => x_axis - y_axis;
+
+        public void print_data(int x, int y)
+        {
+            x_axis += x + 2;
+            y_axis += y + 2;
+            Console.WriteLine(sum() + " " + sub());
+        }
+    }
+
+    // Multi level inheritance
+    public class PRINT : OPERATION
+    {
+        public PRINT(int m, int n) : base(m, n)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Constructor of the print class called");
+            Console.ResetColor();
+        }
+
+        public void print_values()
+        {
+            print_data(10, 5);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Delegate delegateClassObject = new Delegate();
-            Rectangle rectangle = new Rectangle(delegateClassObject.getArea);
-            rectangle += delegateClassObject.getPerimeter;
-
-            rectangle.Invoke(17.77, 40.22);
+            PRINT pr = new PRINT(2, 2);
+            pr.print_values();
         }
     }
 }
