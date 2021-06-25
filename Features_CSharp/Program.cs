f9ee661010719ba5d368b8296ca29aa33bd1765b
Tasnimul Alam <tohiralam153@gmail.com>
2021-06-25 17:45:37 +0600

    Updated

--- Features_CSharp/Program.cs
+++ Features_CSharp/Program.cs
@@ -2,30 +2,57 @@
 
 namespace Features_CSharp
 {
-    // Delegate
-    public delegate void Rectangle(double width, double height);
-    class Delegate
+    abstract class MATH
     {
-        public void getArea(double width, double height)
+        public int x, y;
+        public abstract int sum(int a, int b);
+        public abstract int sub(int a, int b);
+        public abstract int mul(int a, int b);
+        public abstract int div(int a, int b);
+        public void print()
         {
-            Console.WriteLine($"Area of Rectangle: {width * height}");
-        }
-
-        public void getPerimeter(double width, double height)
-        {
-            Console.WriteLine($"Perimeter of Rectangle: {2 * (width * height)}");
+            Console.WriteLine(sum(x, y) + " " + sub(x, y) + " " + mul(x, y) + " " + div(x, y));
         }
     }
 
+    class IMPL : MATH
+    {
+        public IMPL(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public override int sum(int x, int y) => x + y;
+        public override int sub(int x, int y) => x - y;
+        public override int mul(int x, int y) => x * y;
+        public override int div(int x, int y) => x / y;
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Delegate delegateClassObject = new Delegate();
-            Rectangle rectangle = new Rectangle(delegateClassObject.getArea);
-            rectangle += delegateClassObject.getPerimeter;
 
-            rectangle.Invoke(17.77, 40.22);
+            /*
+                                     Access Modifier
+                1. public: The code is accessible for all classes
+                2. private: The code is only accessible within the same class 
+                3. protected: The code is accessible within the same class or
+                   in a class that is inherited from that class. 
+                4. The code is only accessible within its own assembly, but not
+                   from another assembly
+             */
+
+            /*
+             *                         Abstract Class Info.
+             * 
+             * 1. Abstract class are the way to achieve abstraction, it hides 
+             *    implementation detail showing functionality only.
+             * 2. Abstract class can have abstract or non-abstract methods. 
+             * 3. It can't be instantiated, implementation must be provided by derived class
+             */
+            IMPL imp = new IMPL(10, 5);
+            imp.print();
         }
     }
 }
