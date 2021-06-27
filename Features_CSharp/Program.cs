45e9ab4617d665a1273ca34762567831e155e981
Tasnimul Alam <tohiralam153@gmail.com>
2021-06-28 04:52:55 +0600

    Updated

--- Features_CSharp/Program.cs
+++ Features_CSharp/Program.cs
@@ -2,30 +2,51 @@
 
 namespace Features_CSharp
 {
-    // Delegate
-    public delegate void Rectangle(double width, double height);
-    class Delegate
-    {
-        public void getArea(double width, double height)
-        {
-            Console.WriteLine($"Area of Rectangle: {width * height}");
-        }
-
-        public void getPerimeter(double width, double height)
-        {
-            Console.WriteLine($"Perimeter of Rectangle: {2 * (width * height)}");
-        }
-    }
-
+    public delegate void PrintDelegate(string str);
+    public delegate void printValue(int value);
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Delegate delegateClassObject = new Delegate();
-            Rectangle rectangle = new Rectangle(delegateClassObject.getArea);
-            rectangle += delegateClassObject.getPerimeter;
+            /*
+             * 1. Anonymous method can be defined using the delegate keyworkd
+             * 2. It must be assigned to a delegate
+             * 3. It can access out variables or functions
+             * 4. It can be passed as a parameter
+             * 5. These type of methods can be used as event handlers.
+             */
 
-            rectangle.Invoke(17.77, 40.22);
+            PrintDelegate pr = new PrintDelegate(delegate (string str) {
+                Console.WriteLine(str.ToUpper());
+            });
+            pr.Invoke("Hello World");
+
+            PrintDelegate pr2 = delegate (string str)
+            {
+                Console.WriteLine(str.ToLower());
+            };
+            pr2.Invoke("Hello World");
+
+            pr = new PrintDelegate((string str) => Console.WriteLine(str.ToUpper()));
+            pr += (string str) => Console.WriteLine(str.ToLower());
+            pr.Invoke("Hello World");
+
+            // Anonymous method can access variables defined in an outer function
+            int x = 10;
+            printValue pv = new printValue(delegate (int val) { 
+                val += x;
+                Console.WriteLine("The value is " + val);
+            });
+            pv(25);
+
+            // The anonymous method can also be passed to a method that accepts the delegate as a parameter
+            PrintHelperMethod(delegate(int value) { Console.WriteLine("Value: " + value); }, 100);
+        }
+
+        public static void PrintHelperMethod(printValue pv, int value)
+        {
+            value += 10;
+            pv.Invoke(value);
         }
     }
 }
