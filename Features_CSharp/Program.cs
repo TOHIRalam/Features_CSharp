e2175fd6af38188f71aa0b1f4b585ce8621e532a
Tasnimul Alam <tohiralam153@gmail.com>
2021-06-29 22:41:28 +0600

    Update 3.0

--- Features_CSharp/Program.cs
+++ Features_CSharp/Program.cs
@@ -3,29 +3,10 @@ using System.Text;
 
 namespace Features_CSharp
 {
-    // Delegate
-    public delegate void Rectangle(double width, double height);
-    class Delegate
-    {
-        public void getArea(double width, double height)
-        {
-            Console.WriteLine($"Area of Rectangle: {width * height}");
-        }
-
-        public void getPerimeter(double width, double height)
-        {
-            Console.WriteLine($"Perimeter of Rectangle: {2 * (width * height)}");
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            Delegate delegateClassObject = new Delegate();
-            Rectangle rectangle = new Rectangle(delegateClassObject.getArea);
-            rectangle += delegateClassObject.getPerimeter;
-
             /*
              * 1. StringBuilder is used to change or modify the string without occupying
              *    extra memory space on the heap. If we change the original string without 
