93fa0db55944dffd0a943affeb94f8edfd90437e
Tasnimul Alam <tohiralam153@gmail.com>
2021-07-01 05:10:57 +0600

    Update

--- Features_CSharp/Program.cs
+++ Features_CSharp/Program.cs
@@ -2,18 +2,43 @@
 
 namespace Features_CSharp
 {
-    // Delegate
-    public delegate void Rectangle(double width, double height);
-    class Delegate
+    // Generic class
+    class INFO <TData1, TData2>
     {
-        public void getArea(double width, double height)
+        public TData1 Data1 { get; set; }
+        public TData2 Data2 { get; set; }
+
+        public TReturn Data3<TReturn>(TReturn val) => val;
+        public void Data4 <TValue, TKey> (TValue val1, TValue val2, TKey val3)
         {
-            Console.WriteLine($"Area of Rectangle: {width * height}");
+            Console.WriteLine($"{val1} {val2} {val3}");
         }
 
-        public void getPerimeter(double width, double height)
+        public void print() => Console.WriteLine(Data1 + " " + Data2);
+    }
+
+    // Generic Indexer
+    class DATA <TData>
+    {
+        private TData[] store;
+
+        public DATA(int l) => store = new TData[l];
+
+        public TData this[int index]
         {
-            Console.WriteLine($"Perimeter of Rectangle: {2 * (width * height)}");
+            get => store[index];
+            set => store[index] = value;
+        }
+
+        public string this[string index]
+        {
+            get
+            {
+                foreach (var i in store)
+                    if (i.ToString().ToLower() == index.ToLower())
+                        return i + "";
+                return null;
+            }
         }
     }
 
@@ -21,11 +46,34 @@ namespace Features_CSharp
     {
         static void Main(string[] args)
         {
-            Delegate delegateClassObject = new Delegate();
-            Rectangle rectangle = new Rectangle(delegateClassObject.getArea);
-            rectangle += delegateClassObject.getPerimeter;
+            /*
+             * 1. Generic means general form, not specific to a perticular data type.
+             * 2. We define generics with type parameter which is a placeholder for a 
+             *    perticular data type specified when creating an instance of the generic type.
+             * 3. Generic fields in generic class cannot be initialized.
+             */
 
-            rectangle.Invoke(17.77, 40.22);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            // Instantiating Generic Class
+            INFO <string, int> info = new INFO <string, int>();
+
+            info.Data1 = "Hello World!";
+            info.Data2 = info.Data1.Length;
+            Console.WriteLine(info.Data3("Hello"));
+            info.Data4(5, 6, "Hello");
+            info.print();
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            // Instantiating Generic class for generic indexer
+            DATA<int> data1 = new DATA<int>(2);
+            
+            data1[0] = 23;
+            data1[1] = 42;
+
+            Console.Write(data1["42"] + " ");
+            Console.WriteLine(data1[data1[0].ToString()]);
+
+            Console.ResetColor();
         }
     }
 }
