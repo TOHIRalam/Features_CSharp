2376f61b0bc73f64eaebd015ace6576f2131deee
Tasnimul Alam <tohiralam153@gmail.com>
2021-07-01 01:18:43 +0600

    Update 2.0

--- Features_CSharp/Program.cs
+++ Features_CSharp/Program.cs
@@ -6,6 +6,7 @@ namespace Features_CSharp
     {
         int Sum(int x, int y);
         int Sub(int x, int y);
+        string string_data(string funcName1, string funcName2);
     }
 
     interface ISTRING
@@ -17,7 +18,8 @@ namespace Features_CSharp
     {
         public int Sum(int x, int y) => x + y;
         public int Sub(int x, int y) => x - y;
-        public string string_data(string funcName1, string funcName2) => $"{funcName1} and {funcName2} is: ";
+        string IMATH.string_data(string funcName1, string funcName2) => $"{funcName1}/{funcName2}";
+        string ISTRING.string_data(string funcName1, string funcName2) => $"{funcName1} and {funcName2} is: ";
     }
 
     class Program
@@ -30,10 +32,21 @@ namespace Features_CSharp
              * 3. An interface can not contain a constructor.
              * 4. C# does not support multiple inheritance, however it can be achieved with interface, because
              *    the class can implement multiple interfaces. 
+             * 5. We cannot apply access modifier in an interface, all the members are by default public
+             * 6. A class or a Struct can implement one or more interface using colon (:)
+             * 7. An interface can be implemented explicitly using <InterfaceName>.<MemberName>, 
+             *    it is more useful when interfaces have the same method name coincidently.
              */
 
             IMPL impl = new IMPL();
-            Console.Write(impl.string_data("Sum", "Sub") + impl.Sum(10, 8) + " and " + impl.Sub(12, 8));
+            
+            /* When we implement an interface explicitly, we can access interface members only
+               through the instance the instance of an interface type. */ 
+
+            IMATH imath = new IMPL();
+            ISTRING istring = new IMPL();
+            Console.Write($"{imath.string_data("Sum", "Sub")}\n{istring.string_data("Sum", "Sub")}" +
+                $" {impl.Sum(10, 8)} and {impl.Sub(12, 8)}");
         }
     }
 }
