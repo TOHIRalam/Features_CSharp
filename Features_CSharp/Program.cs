862f45fcbe83b90d6f32d1b2984d641a5cedbf0c
Tasnimul Alam <tohiralam153@gmail.com>
2021-07-08 05:34:14 +0600

    16 - Shortcuts

--- Features_CSharp/Program.cs
+++ Features_CSharp/Program.cs
@@ -8,16 +8,20 @@ namespace Features_CSharp
         {
             /* 1. Smart Tags (Ctrl + .)
              * 2. Fore IntelliSense to show (Ctrl + Space)
-             * 3. Search (Ctrl + I)
+             * 3. Search in code (Ctrl + I)
              * 4. Quick info show (Ctrl + K, I)
              * 5. Cut code line (Ctrl + L)
              * 6. Move cursor to above line (Ctrl + Enter)
              * 7. Move selected code above/doewn (Alt + Up/Down)
              * 8. Select Code Vertically (Shift + Alt + Up/Down)
              * 9. Auto format code (Ctrl + K, D)
-             * 10. Comments (Ctrl + K, S)
+             * 10. Comments selected code (Ctrl + K, S)
              * 11. #region (#region + Tab, Tab)
-             * 12. Surround with region (Ctrl + K, S -> select #region) */
+             * 12. Surround with region (Ctrl + K, S -> select #region) 
+             * 13. Go to end bracket (Click on opening bracket -> Ctrl + ])
+             * 14. Navigate to any file (Ctrl + ,)
+             * 15. Debug the application (F5)
+             * 16. Run to cursor (Ctrl + f10) */
         }
     }
 }
