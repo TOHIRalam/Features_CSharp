b34c6d19ed00c9ceb3579135f851108408f54e69
Tasnimul Alam <tohiralam153@gmail.com>
2021-07-14 23:02:13 +0600

    Updated

--- Features_CSharp/Program.cs
+++ Features_CSharp/Program.cs
@@ -4,9 +4,21 @@ namespace Features_CSharp
 {
     class Program
     {
+        static bool checkInput(in int x) => x > 10;
         static void Main(string[] args)
         {
-            
+            /*
+             * 1. The in keyword causes arguments to be passed by reference
+             *    but ensures the argument is not modified. 
+             * 2. When we are using in keyword we don't have to use an in 
+             *    in the caller side.
+             */
+
+            int userInpur = Convert.ToInt32(Console.ReadLine());
+            if(checkInput(userInpur))
+                Console.WriteLine("Greater than 10");
+            else
+                Console.WriteLine("Not greater than 10");
         }
     }
 }
