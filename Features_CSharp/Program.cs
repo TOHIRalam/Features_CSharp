436e929eee7da033a6a231a401431fcecbdd98e8
Tasnimul Alam <tohiralam153@gmail.com>
2021-07-14 22:52:11 +0600

    Updated

--- Features_CSharp/Program.cs
+++ Features_CSharp/Program.cs
@@ -17,11 +17,41 @@ namespace Features_CSharp
             return arr[0];
         }
 
+        static bool ChangeValue(out dynamic x, out dynamic y)
+        {
+            x = "Hello World!";
+            y = 43;
+            return true;
+        }
+
         static void Main(string[] args)
         {
-            // By using out keyword we can return multiple values from methods
+            /* 1. By using out keyword we can return multiple values from methods
+             * 2. Using out keyword any value that is assigned to a variable will be discarded, 
+             *    So, it does not require variable to initialize before passing. 
+             *    
+             *                           ref vs out vs in Modifiers
+             *                          ----------------------------
+             * 1. ref is used to state that the parameter passed maybe modified by the method.
+             * 2. in is used to state that the paramter passed cannot be modified by the method.
+             * 3. out is used to state that the parameter passed must be modified by the method. 
+             * 4. Both ref and in require the parameter to have been initialized before being p
+             *    passed to a method. The out modifier does not require this and is typically not
+             *    initialized prior to being used in a method. 
+             */
+
+            dynamic a = 10, b;
+            ChangeValue(out a, out b);
+            Console.WriteLine(a + " " + b);
+
             int x, y, sum = new Program().maxMinSum(out x, out y, 1, 2, 3, 4, 5);
             Console.WriteLine($"Max: {x}\nMin: {y}\nSum: {sum}\n\n");
+
+            var input = "10x";
+            if (int.TryParse(input, out y))
+                Console.WriteLine("Parsed Value: {0}", y);
+            else
+                Console.WriteLine("Could not parse the variable");
         }
     }
 }
