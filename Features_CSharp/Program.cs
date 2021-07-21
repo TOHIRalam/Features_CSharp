11d5295003c6034f8e50d3062f4a2af9a25c32ad
Tasnimul Alam <tohiralam153@gmail.com>
2021-07-22 03:16:45 +0600

    Updated

--- Features_CSharp/Program.cs
+++ Features_CSharp/Program.cs
@@ -6,7 +6,26 @@ namespace Features_CSharp
     {
         static void Main(string[] args)
         {
-            
+            /* C# provides checked and unchecked keyword to handle integral 
+             * type exceptions. Checked and unchecked keywords specify checked 
+             * context and unchecked context respectively. In checked context, 
+             * arithmetic overflow raises an exception whereas, in an unchecked 
+             * context, arithmetic overflow is ignored and result is truncated. */
+
+            int vall = int.MaxValue;
+            Console.WriteLine(vall + 2);
+
+            checked
+            {
+                int val = int.MaxValue;
+                Console.WriteLine(val + 2);
+            }
+
+            unchecked
+            {
+                int val2 = int.MaxValue;
+                Console.WriteLine(val2 + 2);
+            }
         }
     }
 }
