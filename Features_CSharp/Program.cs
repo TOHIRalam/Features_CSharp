528307ced25bdaf9d694dc90a2727800ce66bbff
Tasnimul Alam <tohiralam153@gmail.com>
2021-07-22 03:32:31 +0600

    Updated

--- Features_CSharp/Program.cs
+++ Features_CSharp/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Features_CSharp
 {
+    class STUDENTS : IComparable<STUDENTS>
+    {
+        public string Name { get; set; }
+        public double CGPA { get; set; }
+        public STUDENTS(string name) => Name = name;
+
+        public int CompareTo(STUDENTS other) => Name.CompareTo(other.Name);
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            
+            List<STUDENTS> studentsList = new List<STUDENTS> {
+                new STUDENTS("Tohir"), new STUDENTS("Alam"),
+                new STUDENTS("Bca"), new STUDENTS("Uvw")
+            };
+            studentsList.Sort();
+            studentsList.ForEach(s => Console.WriteLine(s.Name));
         }
     }
 }
